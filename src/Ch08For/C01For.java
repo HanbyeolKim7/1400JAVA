cb68796710b6958d6ec9434290b3682b40dccf3f
dnflr <dnflr@DESKTOP-JH85PEE>
2022-07-18 16:51:12 +0900

    2022-07-18

---
+++ src/Ch08For/C01For.java
@@ -0,0 +1,42 @@
+package Ch08For;
+
+import java.util.Scanner;
+
+public class C01For {
+
+	public static void main(String[] args) {
+		
+		//int i=1;
+		//while(i<=10)
+		//{
+		//	System.out.println("i : " + i);
+		//	i++;
+		//}
+		//for(int i=1;i<=10;i++) {
+		//	System.out.println("i : " + i);
+		//}
+		
+		
+		//1부터 N까지의 합
+		//N부터 M까지의 합
+		//N부터 M까지의 수 중에 3의 배수를 제외한 합
+		//구구단 출력
+		//------------------------중첩for문-------
+		//전체 구구단 출력
+		//전체 구구단 역순 출력
+		//별찍기(직각삼각형,역직각삼각형,정삼각형,역정삼각형...)
+		
+		//Scanner sc = new Scanner(System.in);
+		//int n = sc.nextInt();
+		//int sum=0;
+		//for(int i=1;i<=n;i++); {
+		//	sum = sum+i;
+		//	System.out.println("합 : " + sum);
+		//}
+		
+		
+		
+		
+	}
+
+}
